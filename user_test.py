7d1aae7a87f6dc089745a4365fd6e22ff9d1dc13
ann-wangui <wanguia822@gmail.com>
2022-04-25 11:28:55 +0300

    change user name

--- user_test.py
+++ user_test.py
@@ -13,15 +13,15 @@ class TestUser(unittest.TestCase):
     
 
     def setUp(self):
-        self.new_user = User("janedoe","j@Z3603",[])
+        self.new_user = User("annwangui","a@Z3603",[])
 
     def tearDown(self) -> None:
         User.user_list = []
 
     #Test1 if user is initialized
     def test_init(self):
-        self.assertEqual(self.new_user.username,"janedoe")
-        self.assertEqual(self.new_user.password,"j@Z3603")
+        self.assertEqual(self.new_user.username,"annwangui")
+        self.assertEqual(self.new_user.password,"a@Z3603")
         self.assertEqual(self.new_user.credentials,[])
 
     #Test2 to check if user is saved
@@ -32,7 +32,7 @@ class TestUser(unittest.TestCase):
     #Test3 to check if we can have multiple users
     def test_save_multiple_users(self):
         self.new_user.save_user()
-        test_user = User("johndoe","J0hzpO3",[])
+        test_user = User("annwangui","annzpO3",[])
         test_user.save_user()
 
         self.assertEqual(len(User.user_list),2)
@@ -40,7 +40,7 @@ class TestUser(unittest.TestCase):
     #Test4 if a user can be deleted
     def test_delete_user(self):
         self.new_user.save_user()
-        test_user = User("johndoe","J0hzpO3",[])
+        test_user = User("annwangui","annzpO3",[])
         test_user.save_user()
         test_user.delete_user()
 
@@ -49,19 +49,19 @@ class TestUser(unittest.TestCase):
     #Test5 Check if user exists
     def test_user_exists(self):
         self.new_user.save_user()
-        test_user = User("johndoe","J0hzpO3",[])
+        test_user = User("annwangui","annzpO3",[])
         test_user.save_user()
 
-        user_exist = User.user_exists("johndoe")
+        user_exist = User.user_exists("annwangui")
         self.assertTrue(user_exist)
 
     #Test6 Find a particular user
     def test_find_user(self):
         self.new_user.save_user()
-        test_user = User("johndoe","J0hzpO3",[])
+        test_user = User("annwangui","annzpO3",[])
         test_user.save_user()
 
-        find_user = User.find_user("johndoe")
+        find_user = User.find_user("annwangui")
         self.assertEqual(find_user, test_user.username)
 
 
