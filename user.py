da23d9ab328e73ec290479676ffe00ab6bcedcb7
ann-wangui <wanguia822@gmail.com>
2022-04-25 09:34:13 +0300

    user is able to create an account

---
+++ user.py
@@ -0,0 +1,34 @@
+from credentials import Credential
+
+class User():
+    user_list =[]
+    #Initialize user
+    def __init__(self,username,password,credentials:Credential):
+        self.username = username
+        self.password = password
+        self.credentials = credentials
+
+    #Save user method
+    def save_user(self):
+        User.user_list.append(self)
+
+    #Delete user method
+    def delete_user(self):
+        User.user_list.remove(self)
+
+    #Check if user exists
+    @classmethod
+    def user_exists(cls,username):
+        for user in cls.user_list:
+            if user.username == username:
+                return True
+        return False
+
+    #Find user
+    @classmethod
+    def find_user(cls,username):
+        for user in cls.user_list:
+            if user.username == username:
+                return username
+
+    pass
