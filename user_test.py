97d898df7bc53c0cab4bf153a43ad761a6884e48
ann-wangui <wanguia822@gmail.com>
2022-04-25 09:33:22 +0300

    user is able to create an account

--- user_test.py
+++ user_test.py
@@ -1,34 +1,69 @@
-from credentials import Credential
+"""
+BDD - Basis of our tests
+1. We should be able to create a user.
+2. Save the user
+    * User has a username and password
+    * Credentials from the credentials class
+"""
 
-class User():
-    user_list =[]
-    #Initialize user
-    def __init__(self,username,password,credentials:Credential):
-        self.username = username
-        self.password = password
-        self.credentials = credentials
+import unittest
+from user import User
 
-    #Save user method
-    def save_user(self):
-        User.user_list.append(self)
+class TestUser(unittest.TestCase):
+    
 
-    #Delete user method
-    def delete_user(self):
-        User.user_list.remove(self)
+    def setUp(self):
+        self.new_user = User("janedoe","j@Z3603",[])
 
-    #Check if user exists
-    @classmethod
-    def user_exists(cls,username):
-        for user in cls.user_list:
-            if user.username == username:
-                return True
-        return False
+    def tearDown(self) -> None:
+        User.user_list = []
 
-    #Find user
-    @classmethod
-    def find_user(cls,username):
-        for user in cls.user_list:
-            if user.username == username:
-                return username
+    #Test1 if user is initialized
+    def test_init(self):
+        self.assertEqual(self.new_user.username,"janedoe")
+        self.assertEqual(self.new_user.password,"j@Z3603")
+        self.assertEqual(self.new_user.credentials,[])
 
-    pass
+    #Test2 to check if user is saved
+    def test_save_user(self):
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+    #Test3 to check if we can have multiple users
+    def test_save_multiple_users(self):
+        self.new_user.save_user()
+        test_user = User("johndoe","J0hzpO3",[])
+        test_user.save_user()
+
+        self.assertEqual(len(User.user_list),2)
+
+    #Test4 if a user can be deleted
+    def test_delete_user(self):
+        self.new_user.save_user()
+        test_user = User("johndoe","J0hzpO3",[])
+        test_user.save_user()
+        test_user.delete_user()
+
+        self.assertEqual(len(User.user_list),1)
+
+    #Test5 Check if user exists
+    def test_user_exists(self):
+        self.new_user.save_user()
+        test_user = User("johndoe","J0hzpO3",[])
+        test_user.save_user()
+
+        user_exist = User.user_exists("johndoe")
+        self.assertTrue(user_exist)
+
+    #Test6 Find a particular user
+    def test_find_user(self):
+        self.new_user.save_user()
+        test_user = User("johndoe","J0hzpO3",[])
+        test_user.save_user()
+
+        find_user = User.find_user("johndoe")
+        self.assertEqual(find_user, test_user.username)
+
+
+if __name__ == '__main__':
+    unittest.main()
